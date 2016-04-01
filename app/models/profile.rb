41e245204f2c85701c3be1797967135920e2124a
Michael Ngo <michael30992@gmail.com>
2016-04-01 18:29:56 +0000

    added profile form and create action

---
+++ app/models/profile.rb
@@ -0,0 +1,8 @@
+class Profile < ActiveRecord::Base
+    belongs_to :user
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :job_title, presence: true
+    validates :phone_number, presence: true
+    validates :contact_email, presence: true
+end
