45b965ea001f162e9e26ea25dfa6b571ed85d85d
Michael Ngo <michael30992@gmail.com>
2016-04-01 16:47:13 +0000

    Final working in c9 stripe_integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,8 @@ class User < ActiveRecord::Base
   attr_accessor :stripe_card_token
   def save_with_payment
     if valid?
-      puts "Creating customer"
       customer = Stripe::Customer.create(email: email, plan: plan_id, card: stripe_card_token)
-      puts "Customer ID:"
-      customer.id.inspect
       self.stripe_customer_token = customer.id
-      puts "stripe_customer_token: "
-      self.stripe_customer_token.inspect
       save!
     end
   end
